bf4a50da574a75cffe1ab68af95f81729109ae89
VALVeisG0D <vichaya_chantamoke@hotmail.com>
2015-10-23 03:10:38 -0700

    SQL table and operations
    Table definition and operations on the table so far.

---
+++ src/membersTable.sql
@@ -0,0 +1,15 @@
+/*format of our SQL database */
+
+CREATE TABLE members
+(
+email VARCHAR(40) NOT NULL,
+psswrd VARCHAR(50) NOT NULL,
+points INT NOT NULL
+);
+
+INSERT INTO members (email, psswrd, points)
+VALUES ('gaben@valvesoftware.com', 'gaben', 1337);
+
+UPDATE members
+SET email = newEmail, psswrd = newPsswrd, points = newPoints
+WHERE email = currentEmail;
