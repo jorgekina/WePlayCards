5b88dd831e175bba35910eba25c1d4a9eb45d382
VALVeisG0D <vichaya_chantamoke@hotmail.com>
2015-10-24 09:40:49 -0700

    Minor changes
    Changed some values

--- src/membersTable.sql
+++ src/membersTable.sql
@@ -2,14 +2,14 @@
 
 CREATE TABLE members
 (
-email VARCHAR(40) NOT NULL PRIMARY KEY,
+email VARCHAR(40) NOT NULL UNIQUE,
 psswrd VARCHAR(50) NOT NULL,
-points INT NOT NULL
+points INT DEFAULT 0
 );
 
 INSERT INTO members (email, psswrd, points)
 VALUES ('gaben@valvesoftware.com', 'gaben', 1337);
 
 UPDATE members
-SET email = newEmail, psswrd = newPsswrd, points = newPoints
+SET email = 'gab@valve.com', psswrd = 'gabn', points = 13373
 WHERE email = 'gaben@valvesoftware.com';
