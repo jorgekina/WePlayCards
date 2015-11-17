d521f03d1be1a65c7853a9fd26614def493f1132
VALVeisG0D <vichaya_chantamoke@hotmail.com>
2015-11-16 21:52:49 -0800

    Revert "Revert "Added database password and streamlined the code.""
    This reverts commit 1867854a56a5ea2653c85eeb7419efd70fb13c13.

--- src/dbconnect.php
+++ src/dbconnect.php
@@ -1,12 +1,8 @@
 <?php
-    $connection = mysqli_connect("127.0.0.1", "root", "");
+    $connection = mysqli_connect("127.0.0.1", "root", "<3ofthecards", "WPCDB");
+
 if (!$connection)
 {
 	die('connection problem! -->'.mysql_error());
-}
-
-if (!mysqli_select_db($connection, "WPCDB"))
-{
-	die('database selction failed! -->'.mysql_error());
 }
 ?>
